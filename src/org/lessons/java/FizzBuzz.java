9f2bae62c439ae668977f6eb0e863b5d82058e87
CalendaLucia <calendalucia4@gmail.com>
2023-05-31 15:39:54 +0200

    fine bonus secondo esercizio

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,5 +1,7 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
 
@@ -20,6 +22,34 @@ public class FizzBuzz {
             }
         }
 
+        // Bonus
+        Scanner requestNumbers = new Scanner(System.in);
+        System.out.print("Quanti numeri vuoi stampare? ");
+        int count = requestNumbers.nextInt();
 
+        if (count <= 0 || count >= 1000) {
+            System.out.println("I numeri da stampare devono essere maggiori di 0 e minori di 1000");
+        } else {
+            for (int i = 1; i <= count; i++) {
+                if ((i % 3 == 0) && (i % 5 == 0)) {
+                    System.out.println(i + " FizzBuzz");
+
+                } else if (i % 3 == 0) {
+
+                    System.out.println(i + " Fizz");
+
+                } else if (i % 5 == 0) {
+                    System.out.println(i + " Buzz");
+
+                } else {
+                    System.out.println(i);
+                }
+            }
+
+        }
     }
+
+
 }
+
+
