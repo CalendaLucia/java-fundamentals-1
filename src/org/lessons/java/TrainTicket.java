796a61dcfc3587dccce17f481165cf4c9dcf68e3
CalendaLucia <calendalucia4@gmail.com>
2023-05-31 15:54:53 +0200

    fine bonus primo esercizio

--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -1,5 +1,6 @@
 package org.lessons.java;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class TrainTicket {
@@ -13,20 +14,29 @@ public class TrainTicket {
         System.out.print("Inserisci età del passeggero: ");
         int age = request.nextInt();
 
-        double ticketPrice = kilometres * 0.21;
-        double discount_20 = ticketPrice * 0.2;
-        double discount_40 = ticketPrice * 0.4;
+        //variabili generali
+        double pricePerKilometer = 0.21;
+        double discountMinors = 0.2;
+        double discountOver = 0.4;
+
+        //Operazioni matematiche per apportare sconti e prezzi al biglietto del treno
+        double ticketPrice = kilometres * pricePerKilometer;
+        double discount_20 = ticketPrice * discountMinors;
+        double discount_40 = ticketPrice * discountOver;
         double discountPrice_20 = ticketPrice - discount_20;
         double discountPrice_40 = ticketPrice - discount_40;
-        System.out.println("Il costo del biglietto è: " + ticketPrice + " euro");
+
+        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+
+        System.out.println("Il costo del biglietto è: " + decimalFormat.format(ticketPrice) + " euro");
 
         if (age < 18) {
-            System.out.println("Hai diritto ad uno sconto del 20% sul totale del biglietto. Dovrai pagare solo " + discountPrice_20 + " euro");
+            System.out.println("Hai diritto ad uno sconto del 20% sul totale del biglietto. Dovrai pagare solo " + decimalFormat.format(discountPrice_20) + " euro");
         } else if (age >= 65) {
-            System.out.println("Hai diritto ad uno sconto del 40% sul totale del biglietto. Dovrai pagare solo " + discountPrice_40 + " euro");
+            System.out.println("Hai diritto ad uno sconto del 40% sul totale del biglietto. Dovrai pagare solo " + decimalFormat.format(discountPrice_40) + " euro");
 
         } else {
-            System.out.println("Il totale del biglietto da pagare è: " + ticketPrice + " euro");
+            System.out.println("Il totale del biglietto da pagare è: " + decimalFormat.format(ticketPrice) + " euro");
         }
 
 
